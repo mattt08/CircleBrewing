c8470f1f57ec6dee756ea10830df9bcf50f79e81
Matthew Thompson <matt@wheretoapps.com>
2012-04-10 20:02:56 -0500

    Deleted unused methods - testing git commit function

--- Classes/BarListViewController.m
+++ Classes/BarListViewController.m
@@ -72,21 +72,6 @@
     [self.myTableView deselectRowAtIndexPath:[self.myTableView indexPathForSelectedRow] animated:YES];
 }
 
-- (void)viewDidAppear:(BOOL)animated
-{
-    [super viewDidAppear:animated];
-}
-
-- (void)viewWillDisappear:(BOOL)animated
-{
-    [super viewWillDisappear:animated];
-}
-
-- (void)viewDidDisappear:(BOOL)animated
-{
-    [super viewDidDisappear:animated];
-}
-
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
 {
     // Return YES for supported orientations
